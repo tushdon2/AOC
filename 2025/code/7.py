5aea220a0a6548725fd476d149a8c60946ed1467
tushdon2 <76245823+tushdon2@users.noreply.github.com>
2025-12-19 02:48:20 +0530

    day 7: tachyon splitting

---
+++ 2025/code/7.py
@@ -0,0 +1,45 @@
+from helpers import fetch, function_runner
+
+"""
+The problem doesn't discuss what if two splitters are adjacent in a row, e.g. ".^..^^...."
+Will the new beam start from the nearest adjacent empty space? 
+.|..|.....      ->      .|..|.....           
+.^..^^....              |^||^^|...
+Or will it just stop for the adjacent splitter, assuming splitters work only if the beam comes to it from previous row?  
+.|..|.....      ->      .|..|.....           
+.^..^^....              |^||^^....
+Thankfully, such a case doesn't exist in the example or the test case.
+"""
+
+lines = [[False if ch == '.' else True for ch in line] for line in fetch(2025, 7).strip().split("\n")]
+
+def part1():
+    cnt, h, w = 0, len(lines), len(lines[0])
+    mask = lines[0]
+    for i in range(1, h):
+        bin_and = [a and b for a, b in zip(mask, lines[i])]
+        cnt += sum(bin_and)
+        newmask = [False] * w
+        for j in range(w):
+            if bin_and[j]:
+                if j > 0: newmask[j - 1] = True
+                if j < w - 1: newmask[j + 1] = True
+            elif mask[j]: newmask[j] = True
+        mask = newmask
+    return cnt
+
+
+def part2():
+    h, w = len(lines), len(lines[0])
+    timelines = lines[0]
+    for i in range(1, h):
+        for j in range(w):
+            if lines[i][j] and timelines[j]:
+                if j > 0: timelines[j-1] += timelines[j]
+                if j < w - 1: timelines[j+1] += timelines[j]
+                timelines[j] = 0
+    return sum(timelines)        
+
+
+if __name__ == "__main__":
+    function_runner(part1, part2)
